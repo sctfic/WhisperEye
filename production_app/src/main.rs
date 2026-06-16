539274f8a88ef45b74bb5008a7197197b1396fad
alban lopez <alban.lopez@gmail.com>
2026-06-16 04:58:13 +0200

    feat(child): burger list and revers proxy link

--- production_app/src/main.rs
+++ production_app/src/main.rs
@@ -17,7 +17,7 @@ use std::sync::{Arc, Mutex};
 
 const WHISPEREYE_BOARD:  &str = "1.0";
 const CHIP_TYPE:  &str = "ESP32-S3";
-const FW_VERSION: &str = "1.0.34";
+const FW_VERSION: &str = "1.0.40";
 #[allow(dead_code)]
 const TOTP_SECRET: &str = "Salt-4-Hash-Between-Probe-&-WhisperEye";
 
@@ -1875,6 +1875,9 @@ pub fn set_boot_to_recovery() {
         }
     }
 }
+
+
+
 
 
 
